775f020c88685d89bcf6c9fbeda79573abd49a38
adarsh <rajshahbvm@yahoo.com>
2016-12-10 11:46:55 +0530

    typo error changed

--- app/src/main/java/in/trujobs/dev/trudroid/api/ServerConstants.java
+++ app/src/main/java/in/trujobs/dev/trudroid/api/ServerConstants.java
@@ -72,7 +72,7 @@ public class ServerConstants {
     //reason type
     public static final int INTERVIEW_REJECT_TYPE_REASON = 1;
     public static final int INTERVIEW_NOT_GOING_TYPE_REASON = 2;
-    public static final int INTERVIEW_NOT_SELECED_TYPE_REASON = 3;
+    public static final int INTERVIEW_NOT_SELECTED_TYPE_REASON = 3;
     public static final int CANDIDATE_ETA = 4;
 
     /* API KEYS */
